64f7910c58bac386b762172012a7fd779bf8972e
hopago <132269386+hopago@users.noreply.github.com>
2024-02-09 18:33:22 +0900

    메인 페이지 반응형

--- client/src/pages/_components/Picks.tsx
+++ client/src/pages/_components/Picks.tsx
@@ -1,7 +1,6 @@
 import RecommendBookInformation from "./RecommendBookInformation";
 
 export default function Picks() {
-  {/* TODO: 반응형 */}
   return (
     <div className="recommend-books picks">
       <RecommendBookInformation />
