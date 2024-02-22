dd9317ccd4fa6414026fa0a2f2f27b6892464929
hopago <132269386+hopago@users.noreply.github.com>
2024-02-22 20:10:26 +0900

    비밀번호 복잡성 검사 추가

--- client/src/pages/join/hooks/use-register-form.ts
+++ client/src/pages/join/hooks/use-register-form.ts
@@ -3,19 +3,90 @@ import { useClerk } from "@clerk/clerk-react";
 import { FormEvent, useState } from "react";
 
 import { useNavigate } from "react-router-dom";
+
 import { useEmailVerification } from "./use-email-verification";
+
+const validateEmail = (email: string) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+// 최소 8자리, 대소문자, 숫자, 특수문자 포함
+const validatePassword = (password: string) => {
+  const re =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return re.test(password);
+};
 
 export const useRegisterForm = () => {
   const { client } = useClerk();
 
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  // user-input
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
+
+  const handleUserInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (name === "password" && !validatePassword(value)) {
+      setErrMsg(
+        "비밀번호는 최소 8자리, 대소문자, 숫자, 특수문자를 포함해야 합니다."
+      );
+      setIsError(true);
+    } else {
+      setUser((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+  };
+
+  // match-pwd
+  const [matchPassword, setMatchPassword] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+
+  const onChangeMatchPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMatchPassword(e.target.value);
+  };
+
+  const isMatchedPassword = () => {
+    if (matchPassword === user.password) {
+      setValidMatch(true);
+    } else {
+      setValidMatch(false);
+    }
+  };
+
+  // email-input
   const [emailAddress, setEmailAddress] = useState("");
   const [emailVerificationCode, setEmailVerificationCode] = useState("");
 
+  const onChangeEmailAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmailAddress(e.target.value);
+  };
+  const onChangeEmailVerificationCode = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEmailVerificationCode(e.target.value);
+  };
+
+  const checkValidEmailAddress = (email: string) => {
+    if (!validateEmail(email)) {
+      setIsError(true);
+      setErrMsg("유효하지 않은 이메일 주소입니다.");
+    } else {
+      setEmailAddress(email);
+    }
+  };
+
+  // result-state
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [errMsg, setErrMsg] = useState("");
@@ -28,6 +99,7 @@ export const useRegisterForm = () => {
       },
     });
 
+  // function
   const handleStartEmailVerification = async () => {
     await startEmailVerification();
   };
@@ -52,9 +124,10 @@ export const useRegisterForm = () => {
     setIsLoading(true);
     try {
       await client.signUp.create({
-        firstName,
-        lastName,
-        username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.username,
+        password: user.password,
         emailAddress,
       });
       await handleRegisterMongoDB();
@@ -69,19 +142,20 @@ export const useRegisterForm = () => {
   };
 
   return {
-    username,
-    setUsername,
-    firstName,
-    setFirstName,
-    lastName,
-    setLastName,
+    user,
+    handleUserInput,
+    matchPassword,
+    onChangeMatchPassword,
+    isMatchedPassword,
+    validMatch,
     handleStartEmailVerification,
     handleCompleteEmailVerification,
     handleRegister,
     emailAddress,
-    setEmailAddress,
+    onChangeEmailAddress,
     emailVerificationCode,
-    setEmailVerificationCode,
+    onChangeEmailVerificationCode,
+    checkValidEmailAddress,
     isLoading,
     isError,
     errMsg,
